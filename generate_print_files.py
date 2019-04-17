4ee0fbc5b43d0e3b3511839177a7221c27460477
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-04-17 14:49:00 +0100

    Use pathlib paths, small refactor

--- generate_print_files.py
+++ generate_print_files.py
@@ -1,10 +1,10 @@
 import argparse
 import csv
-import os
 import sys
 from contextlib import ExitStack
 from datetime import datetime
 from itertools import tee
+from pathlib import Path
 from typing import Iterable
 
 TREATMENT_COUNTRY_TO_PRODUCTPACK_CODE = {
@@ -12,37 +12,38 @@ TREATMENT_COUNTRY_TO_PRODUCTPACK_CODE = {
     'W': 'P_IC_ICL2'
 }
 
+ICL_TEMPLATE = ('UAC', 'CASE_REF', 'ADDRESS_LINE1', 'ADDRESS_LINE2', 'ADDRESS_LINE3', 'TOWN_NAME', 'POSTCODE',
+                'PRODUCTPACK_CODE')
 
-def generate_print_files_from_sample_file_path(sample_file_path: str, output_file_location: str):
+
+def generate_print_files_from_sample_file_path(sample_file_path: Path, output_file_location: Path):
     with open(sample_file_path) as sample_file:
         sample_file, sample_file_line_counter = tee(sample_file)
         sample_size = sum(1 for _ in sample_file_line_counter) - 1
         generate_print_files(sample_file, sample_size, output_file_location)
 
 
-def generate_print_files(sample_file: Iterable[str], sample_size: int, output_file_location: str):
+def generate_print_files(sample_file: Iterable[str], sample_size: int, output_file_location: Path):
     print(f'Preparing to generate print files for {sample_size} sample units')
     sample_file_reader = csv.DictReader(sample_file, delimiter=',')
-    productpack_codes = TREATMENT_COUNTRY_TO_PRODUCTPACK_CODE.values()
+    print_file_paths = {productpack_code: output_file_location.joinpath(
+        f'{productpack_code}_{datetime.utcnow().strftime("%Y-%M-%dT%H-%M-%S")}.csv')
+        for productpack_code in TREATMENT_COUNTRY_TO_PRODUCTPACK_CODE.values()}
+
     with ExitStack() as stack:
-        print_files = {
-            productpack_code: stack.enter_context(
-                open(os.path.join(output_file_location,
-                                  f'{productpack_code}_{datetime.utcnow().strftime("%Y-%M-%dT%H-%M-%S")}.csv'), 'w'))
-            for productpack_code in productpack_codes}
-        fieldnames = ('UAC', 'CASE_REF', 'ADDRESS_LINE1', 'ADDRESS_LINE2', 'ADDRESS_LINE3', 'TOWN_NAME', 'POSTCODE',
-                      'PRODUCTPACK_CODE')
-        csv_writers = {productpack_code: csv.DictWriter(print_file, fieldnames=fieldnames, delimiter='|')
+        print_files = {productpack_code: stack.enter_context(open(print_file_path, 'w'))
+                       for productpack_code, print_file_path in print_file_paths.items()}
+        csv_writers = {productpack_code: csv.DictWriter(print_file, fieldnames=ICL_TEMPLATE, delimiter='|')
                        for productpack_code, print_file in print_files.items()}
 
         for count, sample_row in enumerate(sample_file_reader):
             productpack_code = get_productpack_code_for_treatment_code(sample_row['TREATMENT_CODE'])
-            csv_writers[productpack_code].writerow(create_print_file_row(productpack_code, sample_row, fieldnames))
-            if not count % 1000:
-                sys.stdout.write(f'\rProcessed {count} sample units')
+            csv_writers[productpack_code].writerow(create_print_file_row(productpack_code, sample_row, ICL_TEMPLATE))
+            if not (count + 1) % 1000:
+                sys.stdout.write(f'\rProcessed {count + 1} sample units')
                 sys.stdout.flush()
 
-    print(f'\nFinished writing {len(print_files)} print file(s): {[file.name for file in print_files.values()]}')
+    print(f'\nFinished writing {len(print_files)} print file(s): {[file.name for file in print_file_paths.values()]}')
 
 
 def create_print_file_row(productpack_code: str, sample_row: dict, fieldnames: Iterable[str]) -> dict:
@@ -57,8 +58,8 @@ def get_productpack_code_for_treatment_code(treatment_code: str) -> str:
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate initial contact print files from processed sample file')
-    parser.add_argument('sample_file_path', help='path to the sample file', type=str)
-    parser.add_argument('output_file_path', help='path to the directory to write the print files to', type=str)
+    parser.add_argument('sample_file_path', help='path to the sample file', type=Path)
+    parser.add_argument('output_file_path', help='path to the directory to write the print files to', type=Path)
     return parser.parse_args()
 
 
